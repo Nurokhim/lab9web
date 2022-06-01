717e0a215b56f17d068876a83e2d556dbc336d4e
Nurokhim <101801920+Nurokhim@users.noreply.github.com>
2022-06-01 22:18:54 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db = "latihan1"; 
+$conn = mysqli_connect("localhost","root" , "", "latihan1"); 
+if ($conn == false) 
+{ 
+    echo "Koneksi ke server gagal."; 
+    die(); } 
+    ?>
